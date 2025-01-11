cb7d5c94cf09dad8fd4cfce16cbe3a59bcbd0b75
Faiza5050 <faizakanwal5050@gmail.com>
2025-01-12 01:16:14 +0500

    Updated code with changes

---
+++ chapter-2/main.js
@@ -0,0 +1,64 @@
+// Question # 1:
+// var userName;
+
+
+// Question # 2:
+// var myName = "Faiza Kanwal";
+
+
+// // Question # 3:
+
+// // a) Declare a javaScript variable titled message
+// var message;
+
+// // b) Assign “Hello World” to variable message
+// var message = "Hello World";
+
+// // c) Display the message in an alert box
+// alert(message);
+
+
+// // Question # 4:
+
+// // Save student’s bio data in JavaScript variables
+// var studentName = "Jhone Doe";
+// var studentAge = "15 years old";
+// var courseEnroll = "Certified Mobile Application Development";
+
+// // Show the data in alert boxes
+// alert(studentName);
+// alert(studentAge);
+// alert(courseEnroll);
+
+
+// // Question # 5:
+
+// // Display in one alert box
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+
+// // Question # 6:
+
+// var email = "faizakanwal@gmail.com";
+// var message = "My email address is " + email;
+// alert(message);
+
+
+// // Question # 7:
+
+// var book = "A smarter way to learn JavaScript";
+// var message = "I am trying to learn from the Book " + book;
+// alert(message);
+
+
+// // Question # 8:
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+// // Question # 9:
+
+// var specialString = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(specialString);
+// document.write(specialString);
+
+
