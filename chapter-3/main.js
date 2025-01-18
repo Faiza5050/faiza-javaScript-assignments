859f3f16167b52c335d8f022b4b826865664f891
Faiza5050 <faizakanwal5050@gmail.com>
2025-01-18 18:24:53 +0500

    add chapter 3

---
+++ chapter-3/main.js
@@ -0,0 +1,38 @@
+// // Question # 1:
+
+// var age = 15;
+// alert(`I am ${age} years old`);
+
+
+// // Question # 2:
+
+// var visitCount = 1;
+// alert(`You have visited this site ${visitCount} times`);
+
+// visitCount = visitCount + 1;
+// alert(`You have visited this site ${visitCount} times`);
+
+// visitCount = visitCount + 1;
+// alert(`You have visited this site ${visitCount} times`);
+
+// visitCount = visitCount + 1;
+// alert(`You have visited this site ${visitCount} times`);
+
+// visitCount = visitCount + 1;
+// alert(`You have visited this site ${visitCount} times`);
+
+
+// // Question # 3:
+
+// var birthYear = 1990;
+// document.write(`My birth year is ${birthYear}`);
+
+
+// // Question # 4:
+
+// var visiterName = "John Doe";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+
+// var information = visiterName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.";
+// document.write(information);
